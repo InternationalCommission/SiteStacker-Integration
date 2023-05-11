278db25bb11dcbf75fc08aa6f92cff6edc7ef860
InternationalCommission <133237944+InternationalCommission@users.noreply.github.com>
2023-05-11 09:17:43 -0500

    Create config.py
    Stores authentication credentials for integrated systems.

---
+++ config.py
@@ -0,0 +1,10 @@
+# SiteStacker authentication
+SS_API_KEY = 'your_api_key'
+SS_API_SECRET = 'your_api_secret'
+
+# Constant Contact authentication
+CC_CLIENT_ID = 'your_client_id'
+CC_CLIENT_SECRET = 'your_client_secret'
+CC_ACCESS_TOKEN_URL = 'https://idfed.constantcontact.com/as/token.oauth2'
+CC_AUTHORIZATION_URL = 'https://oauth2.constantcontact.com/oauth2/oauth/siteowner/authorize'
+CC_SCOPES = ['contact_data']
