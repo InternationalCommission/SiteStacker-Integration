18409dc26fa71d9db9668e6f4db22857fba7b3d0
InternationalCommission <133237944+InternationalCommission@users.noreply.github.com>
2023-05-11 09:51:53 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 # SiteStacker authentication
-SS_API_KEY = 'your_api_key'
-SS_API_SECRET = 'your_api_secret'
+SS_API_ID = 'l93n5dun'
+SS_API_SECRET = '1d4c87304824c9810f791chnep2f3159190b145d'
 
 # Constant Contact authentication
-CC_CLIENT_ID = 'your_client_id'
-CC_CLIENT_SECRET = 'your_client_secret'
+CC_CLIENT_ID = 'your_client_id_here'
+CC_CLIENT_SECRET = 'your_client_secret_here'
+CC_AUTHORIZATION_URL = 'https://api.cc.email/v3/idfed'
 CC_ACCESS_TOKEN_URL = 'https://idfed.constantcontact.com/as/token.oauth2'
-CC_AUTHORIZATION_URL = 'https://oauth2.constantcontact.com/oauth2/oauth/siteowner/authorize'
+CC_REDIRECT_URI = 'http://localhost:8000/callback'
 CC_SCOPES = ['contact_data']
