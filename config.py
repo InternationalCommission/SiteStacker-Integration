710eaaaed79cad8b35a9882ff9620f43c7c46272
InternationalCommission <133237944+InternationalCommission@users.noreply.github.com>
2023-05-11 09:59:59 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # SiteStacker authentication
-SS_API_ID = 'l93n5dun'
-SS_API_SECRET = '1d4c87304824c9810f791chnep2f3159190b145d'
+SS_API_ID = 'your_api_id_here'
+SS_API_SECRET = 'your_api_secret_here'
 
 # Constant Contact authentication
 CC_CLIENT_ID = 'your_client_id_here'
